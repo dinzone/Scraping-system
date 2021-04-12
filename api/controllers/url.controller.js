56fb63f29f244c3761eca4da0b10d2a081c9f2d6
Elad Grottas <eladgrott@gmail.com>
2021-04-12 12:59:29 +0300

    handle url validation

--- api/controllers/url.controller.js
+++ api/controllers/url.controller.js
@@ -1,10 +1,17 @@
+const { validationResult } = require('express-validator');
+
 const urlService = require('../services/url.service');
 
 async function parseUrl(req, res) {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const { url } = req.body;
-    try{
+    try {
         return res.send(await urlService.parseUrl(url));
-    }catch(err){
+    } catch (err) {
         console.log('error when parse url');
         console.log(err);
         return res.status(500).send('Internal server error');
--- api/routers/url.router.js
+++ api/routers/url.router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
+const { body } = require('express-validator');
+
 const urlController = require('../controllers/url.controller');
 
-router.post('/parse', urlController.parseUrl);
+router.post('/parse',
+    // check if url is valid in body message
+    body('url').isURL().withMessage('url should be in the correct format'),
+    urlController.parseUrl);
 
 module.exports = router;
