767564def7ee818a833dcaf8a70094e81ef7a929
Elad Grottas <eladgrott@gmail.com>
2021-04-12 12:24:51 +0300

    create parser handler

---
+++ api/services/parseHandler.service.js
@@ -0,0 +1,26 @@
+const parser = require('./parse.service');
+
+// handle link and parse it
+function handleLinkParse(link) {
+    // init dictionary of all the links we already parsed
+    let parsedLinks = {};
+    // recursive function in case sub-links has more links
+    function parseLink(linkToParse) {
+        // parse link with the mock parser
+        let parsedLink = parser(linkToParse);
+        // save the link parse result in the dictionary
+        parsedLinks[linkToParse] = parsedLink.html;
+        // run over all the current sub-links
+        parsedLink.links.forEach(l => {
+            // in case we did not parsed the current sub-link
+            if (parsedLinks[l] === undefined) {
+                parseLink(l);
+            }
+        });
+    }
+    parseLink(link);
+    // convert our dictionary to array of links and their parsed html
+    return Object.keys(parsedLinks).map((key) => ({ url: key, html: parsedLinks[key] }));
+}
+
+module.exports = handleLinkParse;
