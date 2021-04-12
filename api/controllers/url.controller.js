6e499623b553ea4cce477ebf92fd09daf1707e6b
Elad Grottas <eladgrott@gmail.com>
2021-04-12 13:43:52 +0300

    edit some comments

--- api/controllers/url.controller.js
+++ api/controllers/url.controller.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const urlService = require('../services/url.service');
 
 async function parseUrl(req, res) {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
+    // check if the request is valid
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
--- api/services/url.service.js
+++ api/services/url.service.js
@@ -5,12 +5,14 @@ const { FixedThreadPool } = require('poolifier');
 
 const { urlModel } = require('../../models/index');
 
+// create worker threads pool that should execute the function on parseHandler.service.js
 const pool = new FixedThreadPool(os.cpus().length, path.join(__dirname, 'parseHandler.service.js'));
 
 async function parseUrl(url) {
+    // parse url and its sub-links
     let parsedUrls = await pool.execute(url);
+    // upsert each url
     await Promise.all(parsedUrls.map((parsedUrl) => {
-        // upsert each url
         return urlModel.updateOne({ url: parsedUrl.url }, parsedUrl, { upsert: true });
     }));
     return parsedUrls;
